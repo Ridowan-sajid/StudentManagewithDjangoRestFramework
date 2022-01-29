24123260cb19ca51e0f5f89e578392d749b67ba2
Ridowan-sajid <sajidridowan7@gmail.com>
2022-01-29 23:35:55 +0600

    urls added

--- student/urls.py
+++ student/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('studentDetails/',views.student_detail,name='studentDetail'),
-    path('studentList/',views.student_list,name='studentList'),
-    path('studentCreate/',views.student_create,name='studentCreate'),
-    path('studentUpdate/',views.student_update,name='studentUpdate'),
-    path('studentDelete/',views.student_delete,name='studentDelete'),
+    path('studentList/',views.get_api,name='StudentList'),
+    path('studentList/<int:pk>',views.get_api,name='StudentDetails'),
 ]
