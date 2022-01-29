661297a15bca28531d9e2114e0c22c9e6856eb25
Ridowan-sajid <sajidridowan7@gmail.com>
2022-01-29 23:33:58 +0600

    newest version of drf added

--- student/views.py
+++ student/views.py
@@ -1,76 +1,54 @@
 import io
-
 from django.db.migrations import serializer
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+
 from .models import Student
 from .serializers import Student_serializer
 
-@csrf_exempt
-def student_create(request):
+@api_view(['GET','POST','PATCH','PUT','DELETE'])
+def get_api(request,pk=None):
+    if request.method=='GET':
+        if pk is not None:
+            stu=Student.objects.get(id=pk)
+            serializer=Student_serializer(stu)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            stu=Student.objects.all()
+            serializer=Student_serializer(stu,many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
     if request.method=='POST':
-        json_data=request.body
-        stud_stream=io.BytesIO(json_data)
-        stud_dictionary=JSONParser().parse(stud_stream)
-        stud_seria=Student_serializer(data=stud_dictionary)
+        serializer = Student_serializer(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'student created'},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-        if stud_seria.is_valid():
-            stud_seria.save()
-            response={'msg':'Student Created'}
-            json_data=JSONRenderer().render(response)
-            return HttpResponse(json_data,content_type='application/json')
-        else:
-            json_data = JSONRenderer().render(stud_seria.errors)
-            return HttpResponse(json_data, content_type='application/json')
+    if request.method=='PATCH':
+        stu = Student.objects.get(id=pk)
+        serializer = Student_serializer(stu,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'student updated'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def student_detail(request):
-    if request.method=="GET":
-        json_data = request.body
-        stud_stream = io.BytesIO(json_data)
-        stud_dictionary = JSONParser().parse(stud_stream)
-        id = stud_dictionary.get('id')
-        stud = Student.objects.get(id=id)
-        stud_seria=Student_serializer(stud)
-        stud_json=JSONRenderer().render(stud_seria.data)
-        return HttpResponse(stud_json,content_type='application/json')
-
-def student_list(request):
-    if request.method=="GET":
-        stud=Student.objects.all()
-        stud_seria=Student_serializer(stud,many=True)
-        stud_json=JSONRenderer().render(stud_seria.data)
-        return HttpResponse(stud_json,content_type='application/json')
-
-@csrf_exempt
-def student_update(request):
     if request.method=='PUT':
-        json_data=request.body
-        stud_stream=io.BytesIO(json_data)
-        stud_dictionary=JSONParser().parse(stud_stream)
-        id=stud_dictionary.get('id')
-        stud=Student.objects.get(id=id)
-        stud_seria=Student_serializer(stud,data=stud_dictionary,partial=True)
+        stu = Student.objects.get(id=pk)
+        serializer = Student_serializer(stu,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'student updated'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        if stud_seria.is_valid():
-            stud_seria.save()
-            response={'msg':'Student Updated'}
-            json_data=JSONRenderer().render(response)
-            return HttpResponse(json_data,content_type='application/json')
-        else:
-            json_data = JSONRenderer().render(stud_seria.errors)
-            return HttpResponse(json_data, content_type='application/json')
-
-@csrf_exempt
-def student_delete(request):
     if request.method=='DELETE':
-        json_data=request.body
-        stud_stream=io.BytesIO(json_data)
-        stud_dictionary=JSONParser().parse(stud_stream)
-        id=stud_dictionary.get('id')
-        stud=Student.objects.get(id=id)
-        stud.delete()
-        response={'msg':'Student Dleted'}
-        json_data=JSONRenderer().render(response)
-        return HttpResponse(json_data,content_type='application/json')
+        stu = Student.objects.get(id=pk)
+        stu.delete()
+        return Response({'msg': 'student deleted'}, status=status.HTTP_200_OK)
+
+
