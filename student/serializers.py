e15c8bd9af4a0dc8d81c2336d6aa03909217a3f5
Ridowan-sajid <sajidridowan7@gmail.com>
2022-01-29 23:20:03 +0600

    remove code

--- student/serializers.py
+++ student/serializers.py
@@ -6,30 +6,12 @@ class Student_serializer(serializers.ModelSerializer):
         model=Student
         fields='__all__'
 
-    # def validate_cgpa(self,value):
-    #     if value>4.00:
-    #        raise serializers.ValidationError("Cgpa cant be greater than 4")
-    #     return value
-
-    def validate(self, attrs):
-        cg=attrs.get('cgpa')
-        if cg > 4.00:
-            raise serializers.ValidationError("Cgpa cant be greater than 4")
-        return attrs
+    def validate_cgpa(self,value):
+        if value>4.00:
+           raise serializers.ValidationError("Cgpa cant be greater than 4")
+        return value
 
 
 
-# class Student_serializer(serializers.Serializer):
-#     name = serializers.CharField(max_length=30)
-#     department = serializers.CharField(max_length=20)
-#     cgpa = serializers.DecimalField(decimal_places=3, max_digits=5)
-#
-#     def create(self, validated_data):
-#         return Student.objects.create(**validated_data)
-#
-#     def update(self, instance, validated_data):
-#         instance.name=validated_data.get('name',instance.name)
-#         instance.department=validated_data.get('department',instance.department)
-#         instance.cgpa=validated_data.get('cgpa',instance.cgpa)
-#         instance.save()
-#         return instance
+
+
