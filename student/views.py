b192c33b1c062bad7b2478063444b9c9254c8075
Ridowan-sajid <sajidridowan7@gmail.com>
2022-01-29 23:50:29 +0600

    remove unnecessary module

--- student/views.py
+++ student/views.py
@@ -1,11 +1,5 @@
-import io
-from django.db.migrations import serializer
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
 from .models import Student
